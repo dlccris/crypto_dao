c3945329d2842bb768d2e860b99989806473bbad
Crista Minns <crisminns@gmail.com>
2023-10-24 09:57:42 -0400

    frontend for dao project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,34 @@
-import '@/styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import "@/styles/globals.css";
+
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+//require('dotenv').config
+
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "CryptoDevs DAO",
+  projectId: "04b5ca2cf00477b098b9bbd8d05c2698",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
